9a8457908d04ab4a6b892873e6defd4fd52950b2
Ubuntu <ubuntu@ip-172-31-92-222.ec2.internal>
2019-04-14 01:43:56 +0000

    first commit

---
+++ commands.sh
@@ -0,0 +1,4 @@
+docker start ea6b97abdda9 #start the original container
+#OR
+docker start nostalgic_johnson #start the original container
+docker-compose -f staging.yml up -d #start the updated container
